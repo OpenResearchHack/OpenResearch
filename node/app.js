fbcb32d05379fbdd763fab8f7a12c3ae44da169c
Quentin de Beauchesne <quentin.debeauchesne@gmail.com>
2016-11-06 11:12:01 +0000

    adding html & bitcoin client signup

--- node/app.js
+++ node/app.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var wagner = require('wagner-core');
+var nunjucks  = require('nunjucks');
+var _  = require('underscore');
 
 require('./models')(wagner);
 
 var app = express();
+
+nunjucks.configure('views', {
+    autoescape : true,
+    express : app,
+    watch: true
+});
+
+app.get('/register', function(req, res) {
+    res.render('register.html');
+});
 
 //wagner.invoke(require('./auth'), { app: app });
 
